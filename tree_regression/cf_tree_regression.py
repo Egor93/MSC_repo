2f1de1ddae3d8c50495181ac98da73688c73a0da
Egor93 <egor93gordeev@gmail.com>
2020-12-13 12:18:06 +0100

    took care of missing skew_l values, added explicit masking of an array

--- tree_regression/cf_tree_regression.py
+++ tree_regression/cf_tree_regression.py
@@ -6,6 +6,7 @@ from sklearn import tree
 from matplotlib.colors import LogNorm
 import xarray as xr
 import time
+import warnings
 
 ###################
 # DATA PREPROCESSING
@@ -21,7 +22,7 @@ class DataPrepro():
         self.eval_fraction=eval_fraction
 
 
-    def read_netcdf4(self):
+    def read_netcdf(self):
 
         filepath = self.curdir + self.fname
         # ds contains 63 fields
@@ -40,33 +41,35 @@ class DataPrepro():
     def missing_values_present(self,var):
         check_result=False
         # boolean array of missing variables
-        missing_values_bool = get_missing_bool(self,var)
+        missing_values_bool = self.get_missing_bool(var)
         # if there is any missing value present
         if True in missing_values_bool:
             check_result=True
 
         return check_result
 
-    def proc_goalvar4(self):
+    def proc_goalvar(self):
         goalvar_arr = self.ds[self.goalvar][:]
         #ravel -> from 3D(nx*ny, 1, 150) to 1D(nx*ny*150)
         self.goalvar_flat = goalvar_arr.ravel()
 
         return None
 
-
-    def proc_inputvars4(self):
+    def proc_inputvars(self):
         # returns X_arr - 2D arr 
         # of shape (n input_vars,nx*ny*150)
         rows = len(self.input_vars)
         columns = self.goalvar_flat.shape[0]
-        X_arr = np.zeros((rows, columns))
+        # Create !masked! array to be filled.
+        # otherwise mask will be lost while filling X_arr
+        X_arr = np.ma.zeros((rows, columns))
         for index, var in enumerate(self.input_vars):
-            if missing_values_present(self,var):
+            if self.missing_values_present(var):
                 # explicit masking of missing values
                 # + print a user warning
+                warnings.warn("Missing values in {} field were masked!".format(var))
                 var_unmasked = np.array(self.ds[var])
-                missing_values_bool = get_missing_bool(self, var)
+                missing_values_bool = self.get_missing_bool( var)
                 var_arr=np.ma.masked_array(var_unmasked,missing_values_bool)
             else:
                 var_arr = self.ds[var][:]
@@ -77,8 +80,7 @@ class DataPrepro():
 
         return None
 
-
-    def proc_addvars4(self):
+    def proc_addvars(self):
         assert len(self.add_vars) == 2
         qvlm_arr = self.ds[self.add_vars[0]][:]
         qvlm_arr_flat = qvlm_arr.ravel()
@@ -147,13 +149,13 @@ def main():
     prepro=DataPrepro(curdir,fname,goal_var,input_vars,add_vars,eval_fraction)
     # methods should be in this particular order!!
     # prepro.read_netcdf()
-    prepro.read_netcdf4()
+    prepro.read_netcdf()
     # prepro.proc_goalvar()
-    prepro.proc_goalvar4()
+    prepro.proc_goalvar()
     # prepro.proc_inputvars()
-    prepro.proc_inputvars4()
+    prepro.proc_inputvars()
     # prepro.proc_addvars()
-    prepro.proc_addvars4()
+    prepro.proc_addvars()
     prepro.split_data()
     processed_data = prepro.get_processed_data()
 
@@ -162,11 +164,11 @@ def main():
 
     # VISUALISATION
     hist_plot(goal_var_pred, goalvar_eval)
-    print('wow')
 
 
 
 if __name__=='__main__':
+
     # DEFAULT PARAMETERS - global variables
     curdir='/home/igor/UNI/Master Project/Script/Data/'
     fname='ncr_pdf_douze_0125deg.nc'
@@ -178,6 +180,9 @@ if __name__=='__main__':
     max_depth_in=None
 
     start_time = time.time()
+    print("=== EXECUTION START, input file={} ===".format(fname))
+
     main()
+
     end_time =time.time()
-    print("--- %s seconds ---" % (start_time - end_time))
+    print("=== EXECUTION FINISH %s seconds ===" % (start_time - end_time))
